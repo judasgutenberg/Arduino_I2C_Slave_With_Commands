e837301bf399f16c2f4ec2222cf5f955426fb615
Gus Mueller <gus@asecular.com>
2026-01-05 13:03:59 -0500

    Refactor EEPROM functions and add new utilities

--- master-slave.h
+++ master-slave.h
@@ -1,14 +1,16 @@
 //header for a library of functions to communicate with the i2cslave
 #ifndef EEPROM_CONFIG_H
 #define EEPROM_CONFIG_H
-
+#include "globals.h"
 #include <stdint.h>
 #include <Wire.h>
 #include "config.h"     // <-- gives access to ci[] and cs[]
+#include "utilities.h"
 
-void saveAllConfigToEEPROM();
-bool loadAllConfigFromEEPROM(bool justDisplay);
 
+void saveAllConfigToEEPROM(uint16_t addr);
+void readDataParsedFromSlaveSerial();
+int loadAllConfigFromEEPROM(int mode, uint16_t addr);
 int readIntFromEEPROM(uint16_t addr);
 long readLongFromEEPROM(uint16_t addr);
 void writeIntToEEPROM(uint16_t addr, int value);
@@ -18,4 +20,13 @@ void testRead();
 void setAddress(uint16_t addr);
 void readStringFromSlaveEEPROM(uint16_t addr, char* buffer, size_t maxLen);
 void readBytesFromSlaveEEPROM(uint16_t addr, char* buffer, size_t maxLen);
+size_t readBytesFromSlaveSerial( char* buffer, size_t maxLen);
+void sendSlaveSerial(String inVal);
+void normalSlaveMode();
+void enableSlaveSerial(int baudSelector);
+void petWatchDog(uint8_t command, uint32_t unixTime);
+uint32_t getParsedSlaveDatum(uint8_t ordinal);
+uint32_t getSlaveConfigItem(uint8_t ordinal);
+void setSlaveConfigItem(uint8_t ordinal, uint16_t value);
+
 #endif
