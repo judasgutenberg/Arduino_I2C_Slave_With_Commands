ca06aa3074a62724cada7f920535a8ce70614f34
Gus Mueller <gus@asecular.com>
2026-01-12 10:34:24 -0500

    Update slave.ino

--- slave.ino
+++ slave.ino
@@ -615,6 +615,7 @@ void handleCommand(byte command, uint32_t value) {
                     }
                 }
                 lastWatchdogPet = millis();
+                dataToSend = lastWatchdogPet;
             }
             break;
     }
